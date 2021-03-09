e71ba91df2e753b97e86e03ff3e684ddfc5f7f1d
safsom <aryalsafal@gmail.com>
2021-03-09 10:55:29 +0800

    updated example program

--- reverse.c
+++ reverse.c
@@ -1,19 +1,20 @@
 #include "mxc.h"
 
 int main(int argc, char *argv[]) {
-    string_t *input = str_init("Safal: 80, Somil: 90");
+    string_t *input = str_init("safal: 80, somil: 90");
     int slice;
 
     stack_t *reversed = stack_init(sizeof(char));
-    slice = str_substr_cstr(input, "Safal");
+    slice = str_substr_cstr(input, "safal");
     
     int len = str_length(input);
     for (int i = slice; i < len - (len - 5); i++) {
         stack_pushchar(reversed, str_at(input, i));
     }
     for (int i = slice; i < len - (len - 5); i++) {
-        printf("%c", stack_popchar(reversed));
+        putc(stack_popchar(reversed), stdout);
     }
+    putc('\n', stdout);
 
     str_free(input);
     stack_free(reversed);
